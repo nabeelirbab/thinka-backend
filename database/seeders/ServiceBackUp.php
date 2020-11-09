43789b6057b6c6b491adf3e0e3691b043f0c5588
johnenrick <johnenrickplenos@gmail.com>
2020-11-09 23:40:30 +0800

    Update ServiceBackUp

--- database/seeders/ServiceBackUp.php
+++ database/seeders/ServiceBackUp.php
@@ -19,9 +19,9 @@ class ServiceBackUp extends Seeder
         $roles = \DB:: table('roles')->get()->toArray();
         $roleAccessLists = \DB:: table('role_access_lists')->get()->toArray();
         // print_r(json_encode($services));
-        echo Storage::disk('local')->put('bu\services.json', json_encode($services));
-        echo Storage::disk('local')->put('bu\service_actions.json', json_encode($serviceActions));
-        echo Storage::disk('local')->put('bu\roles.json', json_encode($roles));
-        echo Storage::disk('local')->put('bu\role_access_lists.json', json_encode($roleAccessLists));
+        echo \Storage::disk('local')->put('bu\services.json', json_encode($services));
+        echo \Storage::disk('local')->put('bu\service_actions.json', json_encode($serviceActions));
+        echo \Storage::disk('local')->put('bu\roles.json', json_encode($roles));
+        echo \Storage::disk('local')->put('bu\role_access_lists.json', json_encode($roleAccessLists));
     }
 }
