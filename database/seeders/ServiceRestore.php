5c57453ee747a104ac37b59a99ce520f93688cbe
johnenrick <johnenrickplenos@gmail.com>
2020-11-09 23:45:55 +0800

    Update ServiceRestore.php

--- database/seeders/ServiceRestore.php
+++ database/seeders/ServiceRestore.php
@@ -21,10 +21,10 @@ class ServiceRestore extends Seeder
       \DB::table('role_access_lists')->truncate();
       \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
       $baseUrl = env('APP_SERVER_LOCATION');
-      $services = json_decode(str_replace('http:\/\/localhost\/intraactiveops\/api\/api-gateway', $baseUrl, Storage::disk('local')->get('bu\services.json')), true);
-      $serviceAction = json_decode(Storage::disk('local')->get('bu\service_actions.json'), true);
-      $roles = json_decode(Storage::disk('local')->get('bu\roles.json'), true);
-      $roleAccessList = json_decode(Storage::disk('local')->get('bu\role_access_lists.json'), true);
+      $services = json_decode(str_replace('http:\/\/localhost\/intraactiveops\/api\/api-gateway', $baseUrl, \Storage::disk('local')->get('bu\services.json')), true);
+      $serviceAction = json_decode(\Storage::disk('local')->get('bu\service_actions.json'), true);
+      $roles = json_decode(\Storage::disk('local')->get('bu\roles.json'), true);
+      $roleAccessList = json_decode(\Storage::disk('local')->get('bu\role_access_lists.json'), true);
       \DB:: table('services') -> insert($services);
       \DB:: table('service_actions') -> insert($serviceAction);
       \DB:: table('roles') -> insert($roles);
