57242be0a72439c1348266297042f1cdc442974c
johnenrick <johnenrickplenos@gmail.com>
2021-05-26 20:12:06 +0800

    Update email message

--- resources/views/welcome-email.blade.php
+++ resources/views/welcome-email.blade.php
@@ -1,5 +1,10 @@
 <html>
   <body>
-    Welcome to Thinka.io <strong>{{$user}}</strong>
+  <p>
+    Thank you for registering at <a href="https://staging.thinka.io"><strong>thinka.io</strong>.</a>
+  </p>
+  <p>
+    Please visit the <a href="https://staging.thinka.io/#/learning"><strong>learning centre</strong></a> to learn more about how <a href="https://staging.thinka.io">thinka.io</a> works
+  </p>
   </body>
 </html>
